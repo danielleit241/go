19270478e38f27f950e2056a0623d2b9027c8f74
danielleit241 <danielleee241@gmail.com>
2026-02-09 00:41:01 +0700

    feat: add initial implementation of main application with arithmetic and leap year functions

---
+++ func/main.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	// var number1, number2 int
+	// var op string
+	// fmt.Print("Enter first number: ")
+	// fmt.Scan(&number1)
+	// fmt.Print("Enter second number: ")
+	// fmt.Scan(&number2)
+	// fmt.Print("Enter operation (+, -, *, /): ")
+	// fmt.Scan(&op)
+	// result := mathOperation(number1, number2, op)
+	// fmt.Printf("The result is: %f\n", result)
+	// printOperation(number1, number2, op)
+
+	// year := time.Now().Year()
+	// printLeapYear(year)
+}
+
+// void function
+func printOperation(number1, number2 int, op string) {
+	switch op {
+	case "+":
+		fmt.Printf("%d + %d = %d\n", number1, number2, number1+number2)
+	case "-":
+		fmt.Printf("%d - %d = %d\n", number1, number2, number1-number2)
+	case "*":
+		fmt.Printf("%d * %d = %d\n", number1, number2, number1*number2)
+	case "/":
+		if number2 != 0 {
+			fmt.Printf("%d / %d = %f\n", number1, number2, float64(number1)/float64(number2))
+		}
+	default:
+		fmt.Println("Invalid operation")
+	}
+}
+
+func printLeapYear(year int) {
+	if isLeapYear(year) {
+		fmt.Printf("%d is a leap year.\n", year)
+	} else {
+		fmt.Printf("%d is not a leap year.\n", year)
+	}
+}
+
+// return function
+func mathOperation(number1, number2 int, op string) float64 {
+	switch op {
+	case "+":
+		return float64(number1 + number2)
+	case "-":
+		return float64(number1 - number2)
+	case "*":
+		return float64(number1 * number2)
+	case "/":
+		if number2 != 0 {
+			return float64(number1) / float64(number2)
+		}
+	}
+	return 0
+}
+
+func isLeapYear(year int) bool {
+	return (year%4 == 0 && year%100 != 0) || (year%400 == 0)
+}
