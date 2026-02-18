90345f771c69322f5b074a4f7fc4ea9c0317fd7e
danielleit241 <danielleee241@gmail.com>
2026-02-18 23:13:53 +0700

    feat: enhance health check and hello endpoints with JSON responses

--- go-gin/net-http/main.go
+++ go-gin/net-http/main.go
@@ -1,28 +1,60 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
 
 func healthCheckHandler(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Health check OK")
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	response := map[string]string{"status": "ok"}
+	w.Header().Set("Content-Type", "application/json")
+
+	data, err := json.Marshal(response)
+	if err != nil {
+		http.Error(w, "Internal Server Error", http.StatusInternalServerError)
+		return
+	}
+	w.Write(data)
+}
+
+func helloHandler(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodGet {
+		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+		return
+	}
+	response := map[string]string{
+		"message": "Hello Daniel",
+		"user":    "Daniel",
+		"status":  "success",
+	}
+	w.Header().Set("Content-Type", "application/json")
+
+	json.NewEncoder(w).Encode(response) // This is a more concise way to write the JSON response, and it also handles errors internally.
 }
 
 func main() {
 	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
+			return
+		}
 		fmt.Fprintf(w, "Ok")
 	})
 
-	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintf(w, "Hello Daniel")
-	})
+	http.HandleFunc("/hello", helloHandler)
 
 	http.HandleFunc("/health", healthCheckHandler)
 
 	log.Println("Starting server on http://localhost:8080")
+	// :8080 = localhost:8080
 	if err := http.ListenAndServe(":8080", nil); err != nil {
+		// fatalf = log the error and exit the program
 		log.Fatalf("Server failed to start: %v", err)
 	}
 }
